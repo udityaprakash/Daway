bb15b3cb5b748e5aaca2d263bbbd1522aa0f85fa
udityaprakash <udityaprakash983@gmail.com>
2023-02-11 19:00:36 +0530

    login and forgot password corrected for universal login and signup

--- componnents/authentications/user/forgetpass.js
+++ componnents/authentications/user/forgetpass.js
@@ -5,6 +5,10 @@ const nodemailer=require("nodemailer");
 const otpGenerator = require('otp-generator');
 const signup = require("./signup");
 require('dotenv').config();
+const user = require("../../databasevariables/alluserdb");
+const instructor = require("../../databasevariables/InstructorDB");
+const admin = require("../../databasevariables/admindb");
+const company = require("../../databasevariables/companyDB");
 var Emailvalidator = require("email-validator");
 var transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
@@ -29,59 +33,113 @@ const result={
         const { email } = req.body;
         try{
           let otp = otpGenerator.generate(6, { upperCaseAlphabets: false, specialChars: false ,lowerCaseAlphabets:false});
-          let indatabaseotpstored= false;
+          let indatabaseotpstored = false;
+          var founduser = await signup.userexist(email);
+          if (founduser){
+            var resu;
+            if(founduser.usertype== 11){
 
+              resu =  await student.find({email:email});
+              if(resu){
+                const updat = await student.findOneAndUpdate({email:email}, {otp:otp , verified:false});
+  
+                console.log("updated for reset password : "+ updat );
+                indatabaseotpstored = true;
+  
+              }else{
+                res.json({
+                  success:false,
+                  msg:"User Not Found"
+                });
+              } 
 
-          if (await signup.studentexist(email)){
-            // console.log("ueser exixt 1");
-            const resu = await student.find({email:email});
-            // console.log("ueser exixt 2");
-            if(resu.length!=0){
-              // console.log("ueser exixt 3");
-              const updat = await student.findOneAndUpdate({email:email}, {otp:otp , verified:false});
-              // console.log("ueser exixt 4");
+            }else if (founduser.usertype== 12){
 
-              console.log("updated for reset password : "+ updat );
-              indatabaseotpstored = true;
+              resu =await instructor.find({email:email});
+              if(resu){
+                const updat = await instructor.findOneAndUpdate({email:email}, {otp:otp , verified:false});
+  
+                console.log("updated for reset password : "+ updat );
+                indatabaseotpstored = true;
+  
+              }else{
+                res.json({
+                  success:false,
+                  msg:"User Not Found"
+                });
+              } 
+
+            }else if(founduser.usertype == 13){
+
+              resu =await company.find({email:email});
+              if(resu){
+                const updat = await company.findOneAndUpdate({email:email}, {otp:otp , verified:false});
+  
+                console.log("updated for reset password : "+ updat );
+                indatabaseotpstored = true;
+  
+              }else{
+                res.json({
+                  success:false,
+                  msg:"User Not Found"
+                });
+              } 
 
             }else{
-              res.json({
-                success:false,
-                msg:"User Not Found"
-              });
-            }   
-          }
-          var mailOptions = {
-                from: 'udityap.davegroup@gmail.com',
-                to: email,
-                subject: 'Reset Password for DAWAY',
-                html: `<div style="max-width: 90%; margin: auto; padding-top: 20px">
-              <p style="margin-bottom: 30px;">Please enter the OTP to get started</p>
-              <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${ otp }</h1></div>`
-          };
-
-          transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                  console.log("not send : "+error);
-                  res.json({success:false,msg:"otp not send to specified email"})
-            } else {
-                  if(indatabaseotpstored){
-                    console.log('Email sent: ' + info.response);
-                    res.json({
-                        success:true,
-                        msg:"OTP send Successfully."
-                    });
-
-                  }else{
-                    res.json({
-                      success:false,
-                      msg:"OTP not stored in database but OTP send to email."
-                    })
-                  }
-        
-                  
+              resu = await admin.find({email:email});
+              if(resu){
+                const updat = await admin.findOneAndUpdate({email:email}, {otp:otp , verified:false});
+  
+                console.log("updated for reset password : "+ updat );
+                indatabaseotpstored = true;
+  
+              }else{
+                res.json({
+                  success:false,
+                  msg:"User Not Found"
+                });
+              } 
             }
-          });  
+            await user.findOneAndUpdate({email:email},{ verified : false });
+            // const resu = await student.find({email:email});
+            var mailOptions = {
+              from: 'udityap.davegroup@gmail.com',
+              to: email,
+              subject: 'Reset Password for DAWAY',
+              html: `<div style="max-width: 90%; margin: auto; padding-top: 20px">
+            <p style="margin-bottom: 30px;">Please enter the OTP to get started</p>
+            <h1 style="font-size: 40px; letter-spacing: 2px; text-align:center;">${ otp }</h1></div>`
+            };
+
+            transporter.sendMail(mailOptions, function(error, info){
+              if (error) {
+                    console.log("not send : "+error);
+                    res.json({success:false,msg:"otp not send to specified email"})
+              } else {
+                    if(indatabaseotpstored){
+                      console.log('Email sent: ' + info.response);
+                      res.json({
+                          success:true,
+                          msg:"OTP send Successfully."
+                      });
+
+                    }else{
+                      res.json({
+                        success:false,
+                        msg:"OTP not stored in database but OTP send to email."
+                      })
+                    }
+          
+                    
+              }
+            });   
+          }else{
+            res.json({
+              success:false,
+              msg:"User does'nt exist"
+            });
+          }
+           
         }catch{
           res.json({
             success:false,
@@ -97,69 +155,207 @@ const result={
     async (req,res)=>{
       const {otp , email} = req.body ;
       if(Emailvalidator.validate(email) && otp && email){
+          let founduser = await signup.userexist(email);
+          if (founduser){
 
-          if (signup.studentexist(email)){
-            const resu = await student.find({ email : email});
-            if(resu.length!=0){
-              if(resu[0].otp == otp){
-                const fin = await student.findOneAndUpdate({email : email},{otp : null , verified : true});
-              
-                res.json({
-                  success:true,
-                  token:resu[0].id,
-                  msg:"user verified successfully",
-                  result:fin
-                });
+            // const resu = await student.find({ email : email});
+            if(founduser.verified){
+              var finduser;
+              if(founduser.usertype == 11){
+
+                finduser = await student.findOne({email:email});
+                if(finduser.otp == otp){
   
-              }else{
-                res.json({success:false,
-                msg:"Invalid OTP"});
+                  const fin = await student.findOneAndUpdate({email : email},{otp : null , verified : true});
+                  res.json({
+                    success:true,
+                    token:fin._id,
+                    msg:"user verified successfully",
+                    result:fin
+                  });
+    
+                }else{
+                  res.json({success:false,
+                  msg:"Invalid OTP"});
+                  
+                }
+      
+              }else if (founduser.usertype == 12){
+      
+                finduser =await instructor.findOne({email:email});
+                if(finduser.otp == otp){
+
+                  const fin = await instructor.findOneAndUpdate({email : email},{otp : null , verified : true});
                 
+                  res.json({
+                    success:true,
+                    token:fin._id,
+                    msg:"user verified successfully",
+                    result:fin
+                  });
+    
+                }else{
+                  res.json({success:false,
+                  msg:"Invalid OTP"});
+                  
+                }
+      
+              }else if(founduser.usertype == 13){
+      
+                finduser =await company.findOne({email:email});
+                if(user.otp == otp){
+
+                  const fin = await company.findOneAndUpdate({email : email},{otp : null , verified : true});
+                
+                  res.json({
+                    success:true,
+                    token:fin._id,
+                    msg:"user verified successfully",
+                    result:fin
+                  });
+    
+                }else{
+                  res.json({success:false,
+                  msg:"Invalid OTP"});
+                  
+                }
+      
+              }else{
+      
+                finduser =await admin.findOne({email:email});
+                if(finduser.otp == otp){
+
+                  const fin = await admin.findOneAndUpdate({email : email},{otp : null , verified : true});
+                
+                  res.json({
+                    success:true,
+                    token:fin._id,
+                    msg:"user verified successfully",
+                    result:fin
+                  });
+    
+                }else{
+                  res.json({success:false,
+                  msg:"Invalid OTP"});
+                  
+                }
               }
-  
-  
-  
+              await user.findOneAndUpdate({email:email},{verified:true});  
             }else{
               res.json({success:false,
-                msg:"user doesn't exist"});
+                msg:"user is already verified exist"});
   
             }
           }else{
             res.json({success:false,
-            msg:"user is already verified"});
+            msg:"user does'nt exist"});
           }
   
   
     }else{
       res.json({success:false,
         msg:"All fields required or invalid email"});
-
-    }
-  },
+        
+      }
+    },
   Set_password: async (req,res)=>{
-    let {id, password}= req.body;
+    let {email , password}= req.body;
           try{
-            const query = await student.find({_id : id});
+            let founduser = await signup.userexist(email);
+            if(founduser){
+              let userj;
+              if(founduser.usertype == 11){
 
-            if(query.length!=0){
-                const salt= parseInt(process.env.SALT);
-                const hashpassword = await bcrypt.hash(password, salt); 
-                const que = await student.findOneAndUpdate({_id:id},{password:hashpassword});
-                  console.log(que);
-                  res.json({
-                    success:true,
-                    token:query[0].id,
-                    msg:"user password reset successfully"
-                  });
+                userj = await student.findOne({email:email});
+                if(userj.verified){
+                    const salt= parseInt(process.env.SALT);
+                    const hashpassword = await bcrypt.hash(password, salt); 
+                    const que = await student.findOneAndUpdate({email:email},{password:hashpassword});
+                      console.log(que);
+                      res.json({
+                        success:true,
+                        token:userj._id,
+                        msg:"user password reset successfully"
+                      });
+              
+                }else{
+                  res.json({success:false,
+                    msg:"This action is not possible."});
+              
+                }
+      
+              }else if (founduser.usertype == 12){
+                
+                userj =await instructor.findOne({email:email});
+                if(userj.verified){
+                  const salt= parseInt(process.env.SALT);
+                  const hashpassword = await bcrypt.hash(password, salt); 
+                  const que = await instructor.findOneAndUpdate({email:email},{password:hashpassword});
+                    console.log(que);
+                    res.json({
+                      success:true,
+                      token:userj._id,
+                      msg:"user password reset successfully"
+                    });
+    
+              }else{
+                res.json({success:false,
+                  msg:"This action is not possible."});
+    
+              }
+      
+              }else if(founduser.usertype == 13){
+      
+                userj =await company.findOne({email:email});
+                if(userj.verified){
+                  const salt= parseInt(process.env.SALT);
+                  const hashpassword = await bcrypt.hash(password, salt); 
+                  const que = await company.findOneAndUpdate({email:email},{password:hashpassword});
+                    console.log(que);
+                    res.json({
+                      success:true,
+                      token:userj._id,
+                      msg:"user password reset successfully"
+                    });
+    
+              }else{
+                res.json({success:false,
+                  msg:"This action is not possible."});
+    
+              }
+      
+              }else{
+      
+                userj =await admin.findOne({email:email});
+                if(userj.verified){
+                  const salt= parseInt(process.env.SALT);
+                  const hashpassword = await bcrypt.hash(password, salt); 
+                  const que = await admin.findOneAndUpdate({email:email},{password:hashpassword});
+                    console.log(que);
+                    res.json({
+                      success:true,
+                      token:userj._id,
+                      msg:"user password reset successfully"
+                    });
+    
+              }else{
+                res.json({success:false,
+                  msg:"This action is not possible."});
+    
+              }
+              }
+              // const query = await student.find({_id : id});
   
+
             }else{
               res.json({success:false,
                 msg:"user doesn't exist"});
-  
+
             }
           }catch(err){
             res.json({success:false,
-            msg:"Some Internal server error"});
+              error:err,
+              msg:"Some Internal server error"});
           }
 
   }
--- componnents/authentications/user/login.js
+++ componnents/authentications/user/login.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt");
 const path=require("../../../path");
 const student= require("../../databasevariables/studentdb");
+const user = require("../../databasevariables/alluserdb");
+const instructor = require("../../databasevariables/InstructorDB");
+const admin = require("../../databasevariables/admindb");
+const company = require("../../databasevariables/companyDB");
 
 const result = {
 
@@ -9,25 +13,45 @@ const result = {
     let { email , password } = req.body;
     if( email && password ){
       email = email.toLowerCase();
-      const result = await student.find({ email: email });
-      console.log(result);
-      if(result != null){
-            const match =await bcrypt.compare(password, result[0].password);
-            if(match){
-              console.log(result[0].verified);
-              if(result[0].verified == true){
+      const founduser = await user.findOne({ email: email });
+      // console.log(result);
+      if(founduser){
+        let userj;
+        if(founduser.usertype == 11){
+
+          userj = await student.findOne({email:email});
+
+        }else if (founduser.usertype == 12){
+
+          userj =await instructor.findOne({email:email});
+
+        }else if(founduser.usertype == 13){
+
+          userj =await company.findOne({email:email});
+
+        }else{
+
+          userj =await admin.findOne({email:email});
+        }
+        const match =await bcrypt.compare(password, userj.password);
+        if(match){
+              // console.log(result[0].verified);
+              if(userj.verified == true){
 
                 res.status(200).json({
-                  status:true,
+                  success:true,
                   msg:"User Exist",
-                  redirecturl:"user/dashboard/:_id"
+                  redirecturl:"user/dashboard/"+userj._id
                 });
                 // res.redirect("dashboard/"+ result[0]._id);
 
               }else{
-                res.redirect("signup/verifyotp/" + email );
-                res.json({success:false,msg:"user not verified yet please verify",
-              redirecturl:"user/signup/verifyotp/<email>"});
+                // res.redirect("signup/verifyotp/" + email );
+                res.json({
+                  success:false,
+                  msg:"user not verified yet please verify",
+                  redirecturl:"user/signup/verifyotp/<email>"
+                });
               }
 
             }else{
